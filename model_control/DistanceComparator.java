7bf9e825b70bc9aa4d2f7a712964eb4320e7bcb7
Anderson Oliveira Mesquita <andersoncastep@gmail.com>
2016-06-29 16:29:04 +0000

    update - removed generics - 29/06/2016 - 13:29

--- model_control/DistanceComparator.java
+++ model_control/DistanceComparator.java
@@ -1,16 +1,17 @@
 package model_control;
 
-import java.util.Comparator;
+import java.util.*;
 
-public class DistanceComparator implements Comparator {
-	public int compare(Object o1, Object o2){
-		if (o1 != null && o2 !=null && o1 instanceof Vertex && o2 instanceof Vertex){
-			Vertex v1= (Vertex) o1;
-			Vertex v2= (Vertex) o2;
+public class DistanceComparator implements Comparator<Vertex> {
+	public int compare(Vertex o1, Vertex o2){
+		if (o1 != null && o2 !=null){
+			Vertex v1= o1;
+			Vertex v2= o2;
 			Integer d1 = v1.distance;
 			Integer d2 = v2.distance;
-			return (d1.compareTo(d2));
-		} 
+
+			return compareTo(d1, d2);
+		}
 		else return 0;
 	}
 }
