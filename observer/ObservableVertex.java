877fad091ee8b26ad28f659f2357a5da0f5fa7a5
Anderson <andersoncastep@gmail.com>
2016-07-09 14:50:58 -0300

    v1.1

--- observer/ObservableVertex.java
+++
@@ -1,7 +0,0 @@
-package observer;
-
-import model.Vertex;
-
-public interface ObservableVertex extends Observable{
-    public Vertex getState();
-}// ObservableVertex
--- observer/Observer.java
+++
@@ -1,5 +0,0 @@
-package observer;
-
-public interface Observer{
-	public void update(ObservableVertex o);
-}// Observer
